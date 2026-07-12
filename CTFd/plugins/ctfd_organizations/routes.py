1744efcf9e14a9f2684ec6873505ad94f3a87368
Aisha <aishamariedelimarollan@gmail.com>
2026-07-13 00:02:44 +0800

    Remove unused get_user_organization function
    Remove commented-out get_user_organization function.

--- CTFd/plugins/ctfd_organizations/routes.py
+++ CTFd/plugins/ctfd_organizations/routes.py
@@ -53,11 +53,11 @@ def get_team_organization(team_id):
     link = TeamOrganizationLinks.query.filter_by(team_id=team_id).first()
     return link.organization if link else None
 
-
-def get_user_organization(user_id):
-    """Returns the Organization a user has personally joined, if any."""
-    membership = OrganizationMembers.query.filter_by(user_id=user_id).first()
-    return membership.organization if membership else None
+# TODO: Requires a TeamOrganizationLinks model (not yet implemented).
+# def get_user_organization(user_id):
+    #"""Returns the Organization a user has personally joined, if any."""
+    #membership = OrganizationMembers.query.filter_by(user_id=user_id).first()
+    #return membership.organization if membership else None
 
 @organizations_bp.route("/organizations")
 def organizations_list():
